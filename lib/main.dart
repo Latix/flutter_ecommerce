4875efa535770506d178398f1120092d8a3729b3
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-24 14:28:29 +0100

    Code Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import './screens/product_details_screen.dart';
-import './widgets/drawer.dart';
-import './widgets/image_carousel.dart';
-import './widgets/horizontal_list.dart';
-import './widgets/products.dart';
+import './screens/home_page_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePageScreen(),
       theme: ThemeData(
         primarySwatch: Colors.red,
         accentColor: Colors.redAccent,
@@ -19,63 +16,4 @@ void main() {
       },
     ),
   );
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Theme.of(context).primaryColor,
-        title: Text('FashApp'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: ListView(
-        children: <Widget>[
-          // Image Carousel
-          ImageCarousel(),
-          // Padding Widget
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Categories'),
-          ),
-          // Horizontal List View Begins Here
-          HorizontalList(),
-          // Padding Widget
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text('Recent Products'),
-          ),
-          Container(
-            height: 320,
-            child: Products(),
-          ),
-        ],
-      ),
-    );
-  }
 }
---
+++ lib/screens/home_page_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../widgets/drawer.dart';
+import '../widgets/image_carousel.dart';
+import '../widgets/horizontal_list.dart';
+import '../widgets/products.dart';
+
+class HomePageScreen extends StatefulWidget {
+  HomePageScreen({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text('FashApp'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: ListView(
+        children: <Widget>[
+          // Image Carousel
+          ImageCarousel(),
+          // Padding Widget
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Categories'),
+          ),
+          // Horizontal List View Begins Here
+          HorizontalList(),
+          // Padding Widget
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('Recent Products'),
+          ),
+          Container(
+            height: 320,
+            child: Products(),
+          ),
+        ],
+      ),
+    );
+  }
+}
