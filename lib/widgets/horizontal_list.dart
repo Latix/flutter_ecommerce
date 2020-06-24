1ee92a10a08b428ed388f9c03e5e0fffed535404
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-24 07:51:41 +0100

    Code Refactoring

--- lib/widgets/horizontal_list.dart
+++ lib/widgets/horizontal_list.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HorizontalList extends StatelessWidget {
-  final List catItems = [
-    ['images/cats/tshirt.png', 'Shirt'],
-    ['images/cats/accessories.png', 'Accessories'],
-    ['images/cats/dress.png', 'Dress'],
-    ['images/cats/formal.png', 'Formal'],
-    ['images/cats/informal.png', 'Informal'],
-    ['images/cats/jeans.png', 'Jeans'],
-    ['images/cats/shoe.png', 'Shoe'],
+  final catItems = [
+    {'image_url': 'images/cats/tshirt.png', 'name': 'Shirt'},
+    {'image_url': 'images/cats/accessories.png', 'name': 'Accessories'},
+    {'image_url': 'images/cats/dress.png', 'name': 'Dress'},
+    {'image_url': 'images/cats/formal.png', 'name': 'Formal'},
+    {'image_url': 'images/cats/informal.png', 'name': 'Informal'},
+    {'image_url': 'images/cats/jeans.png', 'name': 'Jeans'},
+    {'image_url': 'images/cats/shoe.png', 'name': 'Shoe'}
   ];
   
   @override
@@ -21,8 +21,8 @@ class HorizontalList extends StatelessWidget {
         itemCount: catItems.length,
         itemBuilder: (BuildContext ctx, int index) {
           return Category(
-            image_location: catItems[index][0],
-            image_caption: catItems[index][1],
+            imageLocation: catItems[index]['image_url'],
+            imageCaption: catItems[index]['name'],
           );
         },
       ),
@@ -31,10 +31,10 @@ class HorizontalList extends StatelessWidget {
 }
 
 class Category extends StatelessWidget {
-  final String image_location;
-  final String image_caption;
+  final String imageLocation;
+  final String imageCaption;
 
-  Category({this.image_location, this.image_caption});
+  Category({this.imageLocation, this.imageCaption});
 
   @override
   Widget build(BuildContext context) {
@@ -46,14 +46,14 @@ class Category extends StatelessWidget {
           width: 100.0,
           child: ListTile(
             title: Image.asset(
-              image_location,
+              imageLocation,
               width: 100.0,
               height: 50.0,
             ),
             subtitle: Container(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               alignment: Alignment.topCenter,
-              child: Text(image_caption),
+              child: Text(imageCaption),
             ),
           ),
         ),
